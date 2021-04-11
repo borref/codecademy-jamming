c188d397cd3bb28d4f988e0545b1d04e680044a8
Andrés <andres.castaneda@sofatutor.com>
2021-04-11 18:09:20 +0200

    Add renderAction to Track component

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,6 +2,14 @@ import React from 'react';
 import './Track.css';
 
 class Track extends React.Component {
+  renderAction() {
+    return (
+      <button className="Track-action">
+        {this.props.isRemoval ? '-' : '+'}
+      </button>
+    );
+  }
+
   render() {
     return (
       <div className="Track">
@@ -9,7 +17,7 @@ class Track extends React.Component {
           <h3>{this.props.name}</h3>
           <p>{this.props.artist} | {this.props.album}</p>
         </div>
-        <button className="Track-action">+ or - will go here</button>
+        {this.renderAction()}
       </div>
     );
   }
