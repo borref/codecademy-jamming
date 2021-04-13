3e0e4e26b2e7a282e7d6567014b8f1f8f20ee02e
Andrés <andres.castaneda@sofatutor.com>
2021-04-13 21:22:05 +0200

    Finish getAccessToken method

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,9 +1,29 @@
+const CLIENT_ID = '50aa8b7dfcbc46b3b3466eb8d66f66e6';
+const REDIRECT_URI = 'http://localhost:3000/';
+
 let accessToken;
+let expiresIn;
 
 const Spotify = {
   getAccessToken() {
     if (accessToken) {
       return accessToken;
+    }
+
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[0];
+      expiresIn = Number(expiresInMatch[0]);
+
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+      window.history.pushState('Access Token', null, '/');
+
+      return accessToken;
+    } else {
+      const accessUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=playlist-modify-public&redirect_uri=${REDIRECT_URI}`;
+      window.location = accessUrl;
     }
   }
 };
